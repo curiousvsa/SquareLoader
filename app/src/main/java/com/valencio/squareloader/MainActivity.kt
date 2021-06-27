3d3d5089e8df9758a702c1a542bcd163e014259b
vsa15 <valencioaraujo15@gmail.com>
2021-06-27 16:19:10 +0530

    --- layout Changes ---

--- app/src/main/java/com/valencio/squareloader/MainActivity.kt
+++ app/src/main/java/com/valencio/squareloader/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.valencio.squareloader
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import com.valencio.squareloaderanimation.SlidingSquareLoaderView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var loaderView: SlidingSquareLoaderView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /*  val loaderView = findViewById<SlidingSquareLoaderView>(R.id.loader123)
-            loaderView.show()*/
+        loaderView = findViewById(R.id.loader123)
+        //loaderView.show()
+        timerFunction()
+    }
+
+    private fun timerFunction() {
+        object : CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                //call the squareLoader here.
+                loaderView.show()
+            }
+        }.start()
+
     }
 }
 
